75f5ed37b17ca4017a2552e87fe86401af768f0a
Umut Hope Yıldırım <umut475@gmail.com>
2020-02-20 16:02:23 +0300

    Initial Relase CUBBA

---
+++ main.py
@@ -0,0 +1,25 @@
+#gerekliler
+import glob
+import os
+import shutil
+import subprocess
+
+shutil.rmtree("demo")
+os.mkdir('demo')
+shutil.rmtree("pothole_detected")
+os.mkdir('pothole_detected')
+
+#Scriptler
+# import capture_camera
+import export_video
+import classificition
+
+# Take Every one picture
+count = 0
+pictures = glob.glob("demo/*.jpg")
+picturesLenght = len(pictures)
+
+while(picturesLenght>count):
+    classificition.classification("demo/%d.jpg" % count)
+    count += 1
+
